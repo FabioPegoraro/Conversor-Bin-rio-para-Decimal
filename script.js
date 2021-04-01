b0e068f4fad14e5b82ac25154fb7302355cf7054
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2021-04-01 13:31:35 -0300

    Logica do conversão finalizada

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-
 let numero = "";
 
-function converter(){
-    numero = document.getElementById('numBinario').value;
-    document.getElementById('valorDigitado').value = numero
+function converter() {
+    numero = document.getElementById("numBinario").value;
+    let numBinario = []
+
     
+    function inverter(string) {
+        let saida = "";
+        for (let i = string.length - 1; i >= 0; i--) {
+            saida += string[i];
+        }
+
+        for (let i = 0; i < saida.length; i++) {
+            let caractereAtual = saida.charAt(i);
+
+            numBinario.push(caractereAtual);
+        }
+
+        function transDecimal(num) {
+            let decimal = [];
+            let total = ""; // Valor total do numero Decimal
+
+            // Transformando o número Binário para o decimal
+            num.forEach((element, index) => {
+                for (let i = 1; i <= 1; i++) {
+                    let resultado = Math.pow(2, index) * element;
+
+                    decimal.push(resultado);
+                }
+            });
+            // Calculando o número Decimal
+            for (let i = 0; i < decimal.length; i++) {
+                total = parseInt(total + decimal[i]);
+            }
+            document.getElementById("valorDigitado").value = total
+        }
+        transDecimal(numBinario);
     }
+    inverter(numero)
+
+}
 
