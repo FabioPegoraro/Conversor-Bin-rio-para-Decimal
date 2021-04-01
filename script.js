3cf429a6463a4d5e7e908ce491c5f06edbda11d7
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2021-04-01 16:49:35 -0300

    Colocando condições de entrada no input,verifando se está vazia e se os numeros são binários

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let numero = "";
-
 function converter() {
     numero = document.getElementById("numBinario").value;
+    
+    //Verificando se o campo está vazio
+    if (numero == '' ) {
+        alert("Digite um codigo binário. Ex.: 01011101")
+        console.log('primiero if '+numero)    
+    }
+
     let numBinario = []
 
     
-    function inverter(string) {
+    function inverterCaracter(string) {
         let saida = "";
         for (let i = string.length - 1; i >= 0; i--) {
             saida += string[i];
@@ -13,15 +18,20 @@ function converter() {
 
         for (let i = 0; i < saida.length; i++) {
             let caractereAtual = saida.charAt(i);
-
+            
+            //Verificando se o caracterAtuual é 0 ou 1
+            if(caractereAtual != '1' && caractereAtual != '0'){
+                alert("Digite um codigo binário. Ex.: 01011101")
+                return numero = ''
+            }
             numBinario.push(caractereAtual);
         }
 
-        function transDecimal(num) {
+        // Transformando o número Binário para o decimal
+        function transfDecimal(num) {
             let decimal = [];
             let total = ""; // Valor total do numero Decimal
 
-            // Transformando o número Binário para o decimal
             num.forEach((element, index) => {
                 for (let i = 1; i <= 1; i++) {
                     let resultado = Math.pow(2, index) * element;
@@ -29,15 +39,19 @@ function converter() {
                     decimal.push(resultado);
                 }
             });
+
             // Calculando o número Decimal
             for (let i = 0; i < decimal.length; i++) {
                 total = parseInt(total + decimal[i]);
             }
+
             document.getElementById("valorDigitado").value = total
         }
-        transDecimal(numBinario);
+
+        transfDecimal(numBinario);
     }
-    inverter(numero)
+    
+    inverterCaracter(numero)
 
 }
 
